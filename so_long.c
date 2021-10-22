ef546454d21808d780ac2b86a43022ea493b130b
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2021-10-22 22:42:34 +0300

    done w/o bonus task, but needs norm fix

--- so_long.c
+++ so_long.c
@@ -6,62 +6,85 @@
 /*   By: caniseed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 20:32:46 by caniseed          #+#    #+#             */
-/*   Updated: 2021/10/21 20:33:12 by caniseed         ###   ########.fr       */
+/*   Updated: 2021/10/22 22:31:47 by caniseed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int ft_exit(t_main *mapData) //сюда универсальный free
+t_main	*mapDataInit(void)
 {
-	exit (0);
-}
-
-t_main *structInit(void)
-{
-	t_main *mapData;
+	t_main	*mapData;
 
 	mapData = malloc(sizeof(t_main));
 	mapData->map = NULL;
+	mapData->mlx = NULL;
+	mapData->mlx_win = NULL;
 	mapData->player = (t_player *)malloc(sizeof(t_player));
 	mapData->player->x = 0;
 	mapData->player->y = 0;
-	mapData->width = 0;
-	mapData->height = 0;
+	mapData->size_x = 0;
+	mapData->size_y = 0;
+	mapData->collectibles = 0;
+	mapData->num_of_players = 0;
+	mapData->exit = 0;
 	mapData->moves = 0;
 	return (mapData);
 }
 
-void structFree(t_main *mapData)
+void	freeMap(t_main *mapData)
 {
-	mapData->map = NULL;
-	mapData->mlx = NULL;
-	mapData->mlx_win = NULL;
-	mapData->width = 0;
-	mapData->height = 0;
-	free(mapData->player);
+	int	i;
+
+	i = 0;
+	while (i < mapData->size_y)
+	{
+		free(mapData->map[i]);
+		i++;
+	}
+	free(mapData->map);
+}
+
+void	structFree(t_main *mapData)
+{
+	freeMap(mapData);
+	free(mapData->mlx);
+	mapData->size_x = 0;
+	mapData->size_y = 0;
+	mapData->collectibles = 0;
+	mapData->num_of_players = 0;
+	mapData->exit = 0;
+	mapData->moves = 0;
 	mapData->player->x = 0;
 	mapData->player->y = 0;
+	free(mapData->player);
 	free(mapData);
 }
 
-void drawMap(t_main *mapData)
+int	ft_exit(t_main *mapData)
 {
-	int i;
-	int x;
-	int y;
-	int j;
+	structFree(mapData);
+	exit (SUCCESS);
+}
+
+void	drawMap(t_main *mapData)
+{
+	int		i;
+	int		x;
+	int		y;
+	int		j;
 	void	*picWall;
-	void *picFloor;
-	void *picExit;
-	void *picCollectible;
-	int pic_width;
-	int pic_height;
+	void	*picFloor;
+	void	*picExit;
+	void	*picCollectible;
+	int		pic_width;
+	int		pic_height;
 
 	i = 0;
 	j = 0;
 	x = 0;
 	y = 0;
+	//y = 32;
 	picWall = mlx_xpm_file_to_image(mapData->mlx, WALL, &pic_width, &pic_height);
 	picFloor = mlx_xpm_file_to_image(mapData->mlx, FLOOR, &pic_width, &pic_height);
 	picExit = mlx_xpm_file_to_image(mapData->mlx, EXIT, &pic_width, &pic_height);
@@ -101,22 +124,22 @@ void drawMap(t_main *mapData)
 	}
 }
 
-void putPlayer(t_main *mapData)
+void	putPlayer(t_main *mapData)
 {
-	int pic_width;
-	int pic_height;
-	void *picPlayer;
+	int		pic_width;
+	int		pic_height;
+	void	*picPlayer;
 
-	picPlayer= mlx_xpm_file_to_image(mapData->mlx, PLAYER_1, &pic_width, &pic_height);
+	picPlayer = mlx_xpm_file_to_image(mapData->mlx, PLAYER_1, &pic_width, &pic_height);
 	mlx_put_image_to_window(mapData->mlx, mapData->mlx_win, picPlayer, mapData->player->x * 32, mapData->player->y * 32);
 }
 
-int getMapHeight(char *mapInput)
+int	getMapHeight(char *mapInput)
 {
-	int n;
-	int fd;
-	int counter;
-	char *line;
+	int		n;
+	int		fd;
+	int		counter;
+	char	*line;
 
 	n = 1;
 	counter = 0;
@@ -130,11 +153,11 @@ int getMapHeight(char *mapInput)
 	return (counter);
 }
 
-int getMapWidth(char *mapInput)
+int	getMapWidth(char *mapInput)
 {
-	int length;
-	int fd;
-	char *line;
+	int		length;
+	int		fd;
+	char	*line;
 
 	length = 0;
 	fd = open(mapInput, O_RDONLY);
@@ -144,14 +167,14 @@ int getMapWidth(char *mapInput)
 	return (length);
 }
 
-void locatePlayer(t_main *mapData)
+void	locatePlayer(t_main *mapData)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 0;
-	while(mapData->map[i])
+	while (mapData->map[i])
 	{
 		while (mapData->map[i][j])
 		{
@@ -159,7 +182,48 @@ void locatePlayer(t_main *mapData)
 			{
 				mapData->player->x = j;
 				mapData->player->y = i;
+				mapData->num_of_players++;
 			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+}
+
+void	countCollectibles(t_main *mapData)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (mapData->map[i])
+	{
+		while (mapData->map[i][j])
+		{
+			if (mapData->map[i][j] == 'C')
+				mapData->collectibles++;
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+}
+
+void	countExit(t_main *mapData)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (mapData->map[i])
+	{
+		while (mapData->map[i][j])
+		{
+			if (mapData->map[i][j] == 'E')
+				mapData->exit++;
 			j++;
 		}
 		i++;
@@ -173,25 +237,82 @@ void	key_hook(int keycode, t_main *mapData)
 	{
 		mlx_clear_window(mapData->mlx, mapData->mlx_win);
 		mlx_destroy_window(mapData->mlx, mapData->mlx_win);
-		//free
+		structFree(mapData);
 		exit(SUCCESS);
 	}
 	if (keycode == W)
-		if (mapData->map[mapData->player->y - 1][mapData->player->x] != '1')
+		if (mapData->map[mapData->player->y - 1][mapData->player->x] != '1') {
+			if (mapData->map[mapData->player->y - 1][mapData->player->x] == 'C')
+			{
+				mapData->map[mapData->player->y - 1][mapData->player->x] = '0';
+				mapData->collectibles--;
+			}
+			if (mapData->collectibles == 0 && mapData->map[mapData->player->y - 1][mapData->player->x] == 'E')
+			{
+				mlx_clear_window(mapData->mlx, mapData->mlx_win);
+				mlx_destroy_window(mapData->mlx, mapData->mlx_win);
+				structFree(mapData);
+				exit(SUCCESS);
+			}
 			mapData->player->y -= 1;
+			mapData->moves++;
+		}
 	if (keycode == S)
-		if (mapData->map[mapData->player->y + 1][mapData->player->x] != '1')
+		if (mapData->map[mapData->player->y + 1][mapData->player->x] != '1') {
+			if (mapData->map[mapData->player->y + 1][mapData->player->x] == 'C')
+			{
+				mapData->map[mapData->player->y + 1][mapData->player->x] = '0';
+				mapData->collectibles--;
+			}
+			if (mapData->collectibles == 0 && mapData->map[mapData->player->y + 1][mapData->player->x] == 'E')
+			{
+				mlx_clear_window(mapData->mlx, mapData->mlx_win);
+				mlx_destroy_window(mapData->mlx, mapData->mlx_win);
+				structFree(mapData);
+				exit(SUCCESS);
+			}
 			mapData->player->y += 1;
+			mapData->moves++;
+		}
 	if (keycode == D)
-		if (mapData->map[mapData->player->y][mapData->player->x + 1] != '1')
+		if (mapData->map[mapData->player->y][mapData->player->x + 1] != '1') {
+			if (mapData->map[mapData->player->y][mapData->player->x + 1] == 'C')
+			{
+				mapData->map[mapData->player->y][mapData->player->x + 1] = '0';
+				mapData->collectibles--;
+			}
+			if (mapData->collectibles == 0 && mapData->map[mapData->player->y][mapData->player->x + 1] == 'E')
+			{
+				mlx_clear_window(mapData->mlx, mapData->mlx_win);
+				mlx_destroy_window(mapData->mlx, mapData->mlx_win);
+				structFree(mapData);
+				exit(SUCCESS);
+			}
 			mapData->player->x += 1;
+			mapData->moves++;
+		}
 	if (keycode == A)
-		if (mapData->map[mapData->player->y][mapData->player->x - 1] != '1')
+		if (mapData->map[mapData->player->y][mapData->player->x - 1] != '1') {
+			if (mapData->map[mapData->player->y][mapData->player->x - 1] == 'C')
+			{
+				mapData->map[mapData->player->y][mapData->player->x - 1] = '0';
+				mapData->collectibles--;
+			}
+			if (mapData->collectibles == 0 && mapData->map[mapData->player->y][mapData->player->x - 1] == 'E')
+			{
+				mlx_clear_window(mapData->mlx, mapData->mlx_win);
+				mlx_destroy_window(mapData->mlx, mapData->mlx_win);
+				structFree(mapData);
+				exit(SUCCESS);
+			}
 			mapData->player->x -= 1;
-	printf("%d\n", keycode);
+			mapData->moves++;
+		}
+	printf("Your score is: %d.\n", mapData->moves);
+	//printf("%d\n", keycode);
 }
 
-int render_next_frame(t_main *mapData)
+int	render_next_frame(t_main *mapData)
 {
 	drawMap(mapData);
 	putPlayer(mapData);
@@ -199,7 +320,7 @@ int render_next_frame(t_main *mapData)
 
 int	checkExtension(char *filename)
 {
-	int i;
+	int	i;
 
 	i = (int)ft_strlen(filename) - 3;
 	if (filename[i] == 'b')
@@ -212,18 +333,18 @@ int	checkExtension(char *filename)
 		return (0);
 }
 
-int checkMapSymbols(t_main *mapData)
+int	checkMapSymbols(t_main *mapData)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 0;
-	while(mapData->map[i])
+	while (mapData->map[i])
 	{
 		while (mapData->map[i][j])
 		{
-			if (mapData->map[i][j] == '1' ||mapData->map[i][j] == '0' || mapData->map[i][j] == 'E' ||
+			if (mapData->map[i][j] == '1' || mapData->map[i][j] == '0' || mapData->map[i][j] == 'E' ||
 					mapData->map[i][j] == 'C' || mapData->map[i][j] == 'P')
 				j++;
 			else
@@ -235,63 +356,92 @@ int checkMapSymbols(t_main *mapData)
 	return (1);
 }
 
-int checkMapSize(t_main *mapData, int argc)
+int checkMapSize(t_main *mapData)
 {
-	int i;
+	int	i;
+	int	len;
 
 	i = 1;
-	int len  = (int)ft_strlen(mapData->map[0]);
+	len = (int)ft_strlen(mapData->map[0]);
 	while (mapData->map[i])
 	{
 		if (ft_strlen(mapData->map[i]) != len)
 			return (0);
-		//printf("%d - длина i\n", (int)ft_strlen(mapData->map[i]));
 		i++;
 	}
 	return (1);
 }
 
-int main(int argc, char **argv)
+int	checkWalls(t_main *mapData)
 {
-	t_main *mapData;
-	int fd;
-	int n;
-	int i;
-	int j;
-	if (argc != 2)
+	int	i;
+	int	topWall;
+	int	bottomWall;
+	int	midWall;
+
+	i = 1;
+	topWall = 0;
+	bottomWall = 0;
+	midWall = 0;
+	while (mapData->map[0][topWall] == '1')
+		topWall++;
+	while (mapData->map[mapData->size_y - 1][bottomWall] == '1')
+		bottomWall++;
+	while (i < mapData->size_y - 1)
 	{
-		ft_putstr("Error: wrong number of arguments\n");
-		exit(1);
+		if (mapData->map[i][0] == '1' && mapData->map[i][mapData->size_x - 1] == '1')
+			midWall++;
+		i++;
 	}
-	if (!checkExtension(argv[1]))
-	{
-		ft_putstr("Error: bad file extension\n");
-		exit(1);
-	}
+	if (midWall == mapData->size_y - 2 && topWall == mapData->size_x && bottomWall == mapData->size_x)
+		return (1);
+	else
+		return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_main	*mapData;
+	int		fd;
+	int		n;
+	int		i;
+	int		j;
+
 	n = 1;
 	i = 0;
 	j = 0;
-//	printf("sdrgewgw\n");
-	mapData = structInit();
-	mapData->mlx = mlx_init();
-//	printf("sdrgewgw\n");
-	mapData->height = getMapHeight(argv[1]);
-	mapData->width = getMapWidth(argv[1]);
-//	printf("%d\n", mapData->width);
-//	printf("%d\n", mapData->height);
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0) // добавить проверку на расширение .ber
+	if (argc != 2)
 	{
-		ft_putstr("Error: bad file\n");
+		ft_putstr("Error: wrong number of arguments!\n");
 		exit(1);
 	}
-	mapData->map = (char **)malloc(sizeof(char *) * (mapData->height + 1));
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		ft_putstr("Error: bad file!\n");
+		close(fd);
+		exit(ERROR);
+	}
+	if (!checkExtension(argv[1]))
+	{
+		ft_putstr("Error: bad file extension!\n");
+		close(fd);
+		exit(1);
+	}
+	mapData = mapDataInit();
+	mapData->mlx = mlx_init();
+	mapData->size_y = getMapHeight(argv[1]);
+	mapData->size_x = getMapWidth(argv[1]);
+//	printf("%d\n", mapData->size_x);
+//	printf("%d\n", mapData->size_y);
+	mapData->map = (char **)malloc(sizeof(char *) * (mapData->size_y + 1));
 	if (!mapData->map)
 	{
-		free (mapData->map);
-		exit(1);
+		structFree(mapData);
+		close(fd);
+		exit(ERROR);
 	}
-	ft_bzero(mapData->map, (sizeof(char *) * (mapData->height + 1)));
+	ft_bzero(mapData->map, (sizeof(char *) * (mapData->size_y + 1)));
 	while (n > 0) //getMap
 	{
 		n = get_next_line(fd, &mapData->map[i]);
@@ -300,41 +450,49 @@ int main(int argc, char **argv)
 	close(fd);
 	if (!checkMapSymbols(mapData))
 	{
-		ft_putstr("Error: wrong map\n");
-		//free(mapData); //добавить везде универсальную очистку
-		exit(1);
+		ft_putstr("Error: wrong map!\n");
+		structFree(mapData);
+		exit(ERROR);
 	}
-	if (!checkMapSize(mapData, argc))
+	if (!checkMapSize(mapData))
 	{
-		ft_putstr("Error: wrong map size\n");
-		//free(mapData); //добавить везде универсальную очистку
-		exit(1);
+		ft_putstr("Error: wrong map size!\n");
+		structFree(mapData);
+		exit(ERROR);
+	}
+	if (!checkWalls(mapData))
+	{
+		ft_putstr("Error: map is not surrounded by walls!\n");
+		structFree(mapData);
+		exit(ERROR);
 	}
 	locatePlayer(mapData);
-	printf("%d\n", mapData->player->x);
-	printf("%d\n", mapData->player->y);
-	mapData->mlx_win = mlx_new_window(mapData->mlx, mapData->width * 32, mapData->height * 32, "so_long");
-//	drawMap(mapData);
-	mlx_string_put(mapData->mlx, mapData->mlx_win, 30, 30, 99999, "hello");
+	if (mapData->num_of_players > 1)
+	{
+		ft_putstr("Error: too many players!\n");
+		structFree(mapData);
+		exit(ERROR);
+	}
+	countCollectibles(mapData);
+	if (mapData->collectibles < 1)
+	{
+		ft_putstr("Error: wrong quantity of collectables!\n");
+		structFree(mapData);
+		exit(ERROR);
+	}
+	countExit(mapData);
+	if (mapData->exit < 1)
+	{
+		ft_putstr("Error: wrong quantity of exits!\n");
+		structFree(mapData);
+		exit (ERROR);
+	}
+	//mapData->mlx_win = mlx_new_window(mapData->mlx, mapData->size_x * 32, (mapData->size_y + 1) * 32, "so_long"); //для putstring mlx score
+	mapData->mlx_win = mlx_new_window(mapData->mlx, mapData->size_x * 32, mapData->size_y * 32, "so_long");
+	//mlx_string_put(mapData->mlx, mapData->mlx_win, 50, 50, 99999, ft_itoa(mapData->moves));
 	mlx_key_hook(mapData->mlx_win, key_hook, mapData);
 	mlx_loop_hook(mapData->mlx, render_next_frame, mapData);
-	mlx_hook(mapData->mlx_win, 17, 5, ft_exit, mapData); //red cross exit
-//	printf("%s\n", mapData->map[0]);
-//	printf("%s\n", mapData->map[1]);
-//	printf("%s\n", mapData->map[2]);
-//	printf("%s\n", mapData->map[3]);
-//	printf("%s\n", mapData->map[4]);
-//	printf("%s\n", mapData->map[5]);
-	//mlx_hook(mapData->mlx_win, 17, 0L, ft_exit(), mapData); //red cross exit
-	//mlx_hook(mapData->mlx_win, 17, 5, ft_exit(), mapData); //red cross exit
+	//mlx_hook(mapData->mlx_win, 17, 5, ft_exit, mapData); //do i need it anymore?
 	mlx_loop(mapData->mlx);
-	free(mapData->mlx);
-	while (j < i) //freeMap, возможна утечка
-	{
-		free(mapData->map[j]);
-		j++;
-	}
-	free(mapData->map);
-	structFree(mapData);
 	return (SUCCESS);
 }
--- so_long.h
+++ so_long.h
@@ -6,54 +6,52 @@
 /*   By: caniseed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 20:32:46 by caniseed          #+#    #+#             */
-/*   Updated: 2021/10/21 20:32:46 by caniseed         ###   ########.fr       */
+/*   Updated: 2021/10/22 19:41:00 by caniseed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_SO_LONG_H
-#define SO_LONG_SO_LONG_H
+#ifndef SO_LONG_H
+#define SO_LONG_H
 
 # define BUFFER_SIZE 1
 # define SUCCESS 0
-#define PLAYER_1 "./textures/player_new.xpm"
-#define WALL "./textures/new_wall.xpm"
-#define FLOOR "./textures/new_floor.xpm"
-#define EXIT "./textures/exit.xpm"
-#define COLLECTIBLE "./textures/coin.xpm"
-#define ESC 53
-#define W 13
-#define A 0
-#define S 1
-#define D 2
+# define ERROR 1
+# define PLAYER_1 "./textures/player_new.xpm"
+# define WALL "./textures/new_wall.xpm"
+# define FLOOR "./textures/new_floor.xpm"
+# define EXIT "./textures/exit.xpm"
+# define COLLECTIBLE "./textures/coin.xpm"
+# define ESC 53
+# define W 13
+# define A 0
+# define S 1
+# define D 2
 
-#include "./libft/libft.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include "./mlx/mlx.h"
-//#include <mlx.h>
-
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
+# include "./libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "./mlx/mlx.h"
 
 typedef struct s_player
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 }				t_player;
 
-typedef struct	s_main
+typedef struct s_main
 {
-	char **map;
-	void *mlx;
-	void *mlx_win;
-	int width;
-	int height;
-	int moves;
-	t_player *player;
+	char		**map;
+	void		*mlx;
+	void		*mlx_win;
+	int			size_x;
+	int			size_y;
+	int			collectibles;
+	int			num_of_players;
+	int			exit;
+	int			moves;
+	t_player	*player;
 }				t_main;
 
 size_t	strlen_gnl(char *s);
@@ -62,5 +60,4 @@ int		get_next_line(int fd, char **line);
 char	*clear_saved_buf(char *saved_buf);
 char	*get_line(char *saved_buf);
 
-
-#endif //SO_LONG_SO_LONG_H
+#endif
